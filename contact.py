759b3837dc81989888a98e4c1abc497fc9c0a96f
Justcoderguy <zakharepash@gmail.com>
2018-09-24 17:35:00 +0300

    Merged test methods into one method. Merged test data init classes into one class. Refactored test data init file name.

--- contact.py
+++ contact.py
@@ -1,8 +1,10 @@
 _author_ = 'pzqa'
 
 
-class GroupPrimalFields:
-    def __init__(self, f_name, m_name, l_name, n_name, title, company, address_one):
+class Contact:
+    def __init__(self, f_name, m_name, l_name, n_name, title, company, address_one, home, mobile, work, fax,
+                 email_one, email_two, email_three, homepage, bday, bmonth, byear, aday, amonth, ayear,
+                 address_two, phone_two, notes):
         self.f_name = f_name
         self.m_name = m_name
         self.l_name = l_name
@@ -10,19 +12,10 @@ class GroupPrimalFields:
         self.title = title
         self.company = company
         self.address_one = address_one
-
-
-class GroupPhoneFields:
-    def __init__(self, home, mobile, work, fax):
         self.home = home
         self.mobile = mobile
         self.work = work
         self.fax = fax
-
-
-class GroupWebBdayFields:
-    def __init__(self, email_one, email_two, email_three, homepage,
-                 bday, bmonth, byear, aday, amonth, ayear):
         self.email_one = email_one
         self.email_two = email_two
         self.email_three = email_three
@@ -33,10 +26,6 @@ class GroupWebBdayFields:
         self.aday = aday
         self.amonth = amonth
         self.ayear = ayear
-
-
-class GroupSecondaryFields:
-    def __init__(self, address_two, phone_two, notes):
         self.address_two = address_two
         self.phone_two = phone_two
         self.notes = notes
--- test_add_contact.py
+++ test_add_contact.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import unittest
-from group_contact import GroupPrimalFields, GroupPhoneFields, GroupWebBdayFields, GroupSecondaryFields
+from contact import Contact
 
 
 class TestAddContact(unittest.TestCase):
@@ -21,107 +21,101 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def primal_data_fields(self, wd, group_contact):
+    def primal_data_fields(self, wd, contact):
         # init add contact
         wd.find_element_by_link_text("add new").click()
         # add first name
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group_contact.f_name)
+        wd.find_element_by_name("firstname").send_keys(contact.f_name)
         # add middle name
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(group_contact.m_name)
+        wd.find_element_by_name("middlename").send_keys(contact.m_name)
         # add second name
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group_contact.l_name)
+        wd.find_element_by_name("lastname").send_keys(contact.l_name)
         # add nickname
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(group_contact.n_name)
+        wd.find_element_by_name("nickname").send_keys(contact.n_name)
         # add title
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(group_contact.title)
+        wd.find_element_by_name("title").send_keys(contact.title)
         # add company
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group_contact.company)
+        wd.find_element_by_name("company").send_keys(contact.company)
         # add address
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group_contact.address_one)
-
-    def phone_data_fields(self, wd, group_contact):
+        wd.find_element_by_name("address").send_keys(contact.address_one)
         # add home phone
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(group_contact.home)
+        wd.find_element_by_name("home").send_keys(contact.home)
         # add mobile phone
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(group_contact.mobile)
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
         # add work phone
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(group_contact.work)
+        wd.find_element_by_name("work").send_keys(contact.work)
         # add fax
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(group_contact.fax)
-
-    def web_and_bday_fields(self, wd, group_contact):
+        wd.find_element_by_name("fax").send_keys(contact.fax)
         # add first email
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(group_contact.email_one)
+        wd.find_element_by_name("email").send_keys(contact.email_one)
         # add second email
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(group_contact.email_two)
+        wd.find_element_by_name("email2").send_keys(contact.email_two)
         # add third email
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(group_contact.email_three)
+        wd.find_element_by_name("email3").send_keys(contact.email_three)
         # add homepage address
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(group_contact.homepage)
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
         # select birthday data
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(group_contact.bday)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
         wd.find_element_by_name("bday").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(group_contact.bmonth)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
         wd.find_element_by_name("bmonth").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(group_contact.byear)
+        wd.find_element_by_name("byear").send_keys(contact.byear)
         # select anniversary data
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(group_contact.aday)
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
         wd.find_element_by_name("aday").click()
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(group_contact.amonth)
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
         wd.find_element_by_name("amonth").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(group_contact.ayear)
-
-    def secondary_fields(self, wd, group_contact):
+        wd.find_element_by_name("ayear").send_keys(contact.ayear)
         # add second address
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(group_contact.address_two)
+        wd.find_element_by_name("address2").send_keys(contact.address_two)
         # add second phone
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(group_contact.phone_two)
+        wd.find_element_by_name("phone2").send_keys(contact.phone_two)
         # add notes
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(group_contact.notes)
+        wd.find_element_by_name("notes").send_keys(contact.notes)
 
     def submit_contact(self, wd):
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
@@ -133,16 +127,15 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.primal_data_fields(wd, GroupPrimalFields(f_name='John', m_name='Smith', l_name='Doe', n_name='Anonymous',
-                                                      title='worker', company='Microsoft', address_one='Redwood 11'))
-        self.phone_data_fields(wd, GroupPhoneFields(home='7777-777-777', mobile='004911234567890',
-                                                    work='5555-555-555', fax='12315135'))
-        self.web_and_bday_fields(wd, GroupWebBdayFields(email_one='mail@domaim.com', email_two='mail@domain.com.de',
-                                                        email_three='mail@subdomain.domain.com',
-                                                        homepage='http:/example.com', bday='1', bmonth='January',
-                                                        byear='1975', aday='12', amonth='November', ayear='2010'))
-        self.secondary_fields(wd, GroupSecondaryFields(address_two='Bluewaters 123', phone_two='+1234567890',
-                                                       notes='Worker of the year.'))
+        self.primal_data_fields(wd, Contact(f_name='John', m_name='Smith', l_name='Doe', n_name='Anonymous',
+                                            title='worker', company='Microsoft', address_one='Redwood 11',
+                                            home='7777-777-777', mobile='004911234567890', work='5555-555-555',
+                                            fax='12315135', email_one='mail@domaim.com',
+                                            email_two='mail@domain.com.de', email_three='mail@subdomain.domain.com',
+                                            homepage='http:/example.com', bday='1', bmonth='January', byear='1975',
+                                            aday='12', amonth='November', ayear='2010',
+                                            address_two='Bluewaters 123', phone_two='+1234567890',
+                                            notes='Worker of the year.'))
         self.submit_contact(wd)
         self.return_to_home_page(wd)
 
@@ -150,16 +143,15 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.primal_data_fields(wd, GroupPrimalFields(f_name='', m_name='', l_name='', n_name='',
-                                                      title='', company='', address_one=''))
-        self.phone_data_fields(wd, GroupPhoneFields(home='', mobile='',
-                                                    work='', fax=''))
-        self.web_and_bday_fields(wd, GroupWebBdayFields(email_one='', email_two='',
-                                                        email_three='',
-                                                        homepage='', bday='', bmonth='-',
-                                                        byear='', aday='', amonth='-', ayear=''))
-        self.secondary_fields(wd, GroupSecondaryFields(address_two='', phone_two='',
-                                                       notes=''))
+        self.primal_data_fields(wd, Contact(f_name='', m_name='', l_name='', n_name='',
+                                            title='', company='', address_one='',
+                                            home='', mobile='', work='',
+                                            fax='', email_one='',
+                                            email_two='', email_three='',
+                                            homepage='', bday='', bmonth='-', byear='',
+                                            aday='', amonth='-', ayear='',
+                                            address_two='', phone_two='',
+                                            notes=''))
         self.submit_contact(wd)
         self.return_to_home_page(wd)
 
