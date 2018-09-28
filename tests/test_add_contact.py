40003084001887b75e80c867c51540007f321e36
Justcoderguy <zakharepash@gmail.com>
2018-09-28 17:11:52 +0300

    Added class helpers.

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -13,8 +13,8 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login("admin", "secret")
-    app.fill_contact_fields(Contact(f_name='John', m_name='Smith', l_name='Doe', n_name='Anonymous',
+    app.session.login("admin", "secret")
+    app.contact.fill_fields(Contact(f_name='John', m_name='Smith', l_name='Doe', n_name='Anonymous',
                                     title='worker', company='Microsoft', address_one='Redwood 11',
                                     home='7777-777-777', mobile='004911234567890', work='5555-555-555',
                                     fax='12315135', email_one='mail@domaim.com',
@@ -23,12 +23,12 @@ def test_add_contact(app):
                                     aday='12', amonth='November', ayear='2010',
                                     address_two='Bluewaters 123', phone_two='+1234567890',
                                     notes='Worker of the year.'))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login("admin", "secret")
-    app.fill_contact_fields(Contact(f_name='', m_name='', l_name='', n_name='',
+    app.session.login("admin", "secret")
+    app.contact.fill_fields(Contact(f_name='', m_name='', l_name='', n_name='',
                                     title='', company='', address_one='',
                                     home='', mobile='', work='',
                                     fax='', email_one='',
@@ -37,4 +37,4 @@ def test_add_empty_contact(app):
                                     aday='', amonth='-', ayear='',
                                     address_two='', phone_two='',
                                     notes=''))
-    app.logout()
+    app.session.logout()
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -13,12 +13,12 @@ def app(request):
 
 
 def test_method_add_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="Group name", logo="Group logo", comment="Group comment"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="Group name", logo="Group logo", comment="Group comment"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
-    app.create_group(Group(name="", logo="", comment=""))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.group.create(Group(name="", logo="", comment=""))
+    app.session.logout()
