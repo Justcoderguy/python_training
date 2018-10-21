c0b579c3cf6184737044074127f1ade2a722bb85
Pavel <zakharepash@gmail.com>
2018-10-21 20:25:27 +0300

    Made add group and add contact tests parametrized.

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,36 +1,41 @@
 # -*- coding: utf-8 -*-
 from models.contact import Contact
+import random
+import string
+import pytest
 __author__ = 'pzqa'
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_contact = [Contact(f_name=f_name, m_name=m_name, l_name=l_name, n_name=n_name,
+                        title=title, company=company, address_one=address_one,
+                        home='7777777777', mobile='4912331321', work='456321345613',
+                        fax='5544341233', email_one='mail@domain.com',
+                        email_two='someser@somedomain.de', email_three='anyuser@anydomain.net',
+                        homepage='www.example.com', bday='1', bmonth='January', byear='1975',
+                        aday='12', amonth='November', ayear='2010',
+                        address_two='user@mail.org', phone_two='4426313451235',
+                        notes=notes)
+                for f_name in ["", random_string("f_name", 10)]
+                for m_name in ["", random_string("m_name", 10)]
+                for l_name in ["", random_string("l_name", 10)]
+                for n_name in ["", random_string("n_name", 10)]
+                for title in ["", random_string("title", 10)]
+                for company in ["", random_string("company", 10)]
+                for address_one in ["", random_string("address_one", 10)]
+                for notes in ["", random_string("notes", 10)]
+                ]
+
+
+@pytest.mark.parametrize("contact", test_contact, ids=[repr(x) for x in test_contact])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(f_name='John', m_name='Smith', l_name='Doe', n_name='Anonymous',
-                      title='worker', company='Microsoft', address_one='Redwood 11',
-                      home='7777-777-777', mobile='004911234567890', work='5555-555-555',
-                      fax='12315135', email_one='mail@domaim.com',
-                      email_two='mail@domain.com.de', email_three='mail@subdomain.domain.com',
-                      homepage='http:/example.com', bday='1', bmonth='January', byear='1975',
-                      aday='12', amonth='November', ayear='2010',
-                      address_two='Bluewaters 123', phone_two='+1234567890',
-                      notes='Worker of the year.')
     app.contact.add_new(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-# def test_add_empty_contact(app):
-#     old_contacts = app.contact.get_contact_list()
-#     app.contact.add_new(Contact(f_name='', m_name='', l_name='', n_name='',
-#                                 title='', company='', address_one='',
-#                                 home='', mobile='', work='',
-#                                 fax='', email_one='',
-#                                 email_two='', email_three='',
-#                                 homepage='', bday='', bmonth='-', byear='',
-#                                 aday='', amonth='-', ayear='',
-#                                 address_two='', phone_two='',
-#                                 notes=''))
-#     new_contacts = app.contact.get_contact_list()
-#     assert len(old_contacts) + 1 == len(new_contacts)
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*-
 from models.group import Group
+import pytest
+import random
+import string
 __author__ = 'pzqa'
 
 
-def test_method_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_group = [Group(name=name, logo=logo, comment=comment)
+              for name in ["", random_string("name", 10)]
+              for logo in ["", random_string("logo", 15)]
+              for comment in ["", random_string("comment", 10)]]
+
+
+@pytest.mark.parametrize("group", test_group, ids=[repr(x) for x in test_group])
+def test_method_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="Group name", logo="Group logo", comment="Group comment")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group = Group(name="", logo="", comment="")
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
